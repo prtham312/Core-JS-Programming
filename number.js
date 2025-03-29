cf431d08bdf179e5b2da871acf8f3a1fff7aab28
prtham312 <pb3488@srmist.edu.in>
2025-03-29 12:25:08 +0530

    round off a number

--- number.js
+++ number.js
@@ -2,3 +2,11 @@
 let num = 35;
 console.log(num.toString());
 console.log(typeof(num.toString()));
+
+//round off a number
+let x = 3.5654
+console.log(x.toFixed());
+console.log(x.toFixed(1));
+console.log(x.toFixed(2));
+console.log(x.toFixed(3));
+console.log(x.toFixed(5));
