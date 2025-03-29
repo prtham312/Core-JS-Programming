2442df05af77de2f338e50057294e1e85e7e5255
prtham312 <pb3488@srmist.edu.in>
2025-03-29 11:13:11 +0530

    Acessing a string

---
+++ Acess_string.js
@@ -0,0 +1,8 @@
+//acessing the strings
+let str1 = "abihbwvvre";
+let res1 = str.charCodeAt(1);
+let res1a = str.at(1);
+let res1b = str.charAt(1);
+console.log(res1);
+console.log(res1a);
+console.log(res1b);
