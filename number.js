427bba801af27f7fcb26100b4dda9ce8db8801bb
prtham312 <pb3488@srmist.edu.in>
2025-03-29 12:43:55 +0530

    Parse string to number

--- number.js
+++ number.js
@@ -25,3 +25,11 @@ console.log(Number("100"))
 console.log(typeof(Number("100")))
 console.log(Number("hello"))
 console.log(Number("10.40"))
+
+
+//parse a string and returns a number but only the first number,  also ignores whitespaces in starting
+console.log(parseInt("   19"))
+console.log(parseInt("18.32"))
+console.log(parseInt("10 20 30"));
+console.log(parseInt("10 years"));
+console.log(parseInt("years 10"));
