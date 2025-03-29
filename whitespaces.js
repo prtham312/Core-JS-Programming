97f2bae94d74873e83f1a3e3a018399742c29342
prtham312 <pb3488@srmist.edu.in>
2025-03-29 11:16:53 +0530

    Remove whitepsaces from string

---
+++ whitespaces.js
@@ -0,0 +1,8 @@
+//to remove white spaces from the string
+let str4 = "                      hello world , here i am removing whitespaces              "
+let res4a = str4.trim();
+let res4b = str4.trimStart();
+let res4c = str4.trimEnd();
+console.log(res4a);
+console.log(res4b);
+console.log(res4c);
