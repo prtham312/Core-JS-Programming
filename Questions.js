e10b785525f2d1c1715dd6e76fb0cb449dc9e879
prtham312 <pb3488@srmist.edu.in>
2025-04-01 10:26:03 +0530

    palindrome check

--- Questions.js
+++ Questions.js
@@ -2,3 +2,9 @@
 let str = "abcd";
 let res = str.split("").reverse().join("");
 console.log(res);
+
+//check if string is palindrome
+let str1 = "abba";
+let res1 = str1.split("").reverse().join("");
+if(str1==res1){ console.log("true");}
+else {console.log("false");}
