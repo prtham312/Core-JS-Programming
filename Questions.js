2c2307b5298ba0a3dab0ef075946406158c027c9
prtham312 <pb3488@srmist.edu.in>
2025-04-01 10:35:37 +0530

    first and last elements of array after sorting

--- Questions.js
+++ Questions.js
@@ -8,3 +8,12 @@ let str1 = "abba";
 let res1 = str1.split("").reverse().join("");
 if(str1==res1){ console.log("true");}
 else {console.log("false");}
+
+//first and last elements of array after sorting
+const arr = [ 2 , 3 , 1 , 4 , 7 , 5 , 9 , 6];
+function sortArray(arr){
+   return arr.sort((a,b) => a-b);
+}
+let n = arr.length;
+sortArray(arr);
+console.log(`First element is ${arr[0]} and last element is ${arr[n-1]}`);
