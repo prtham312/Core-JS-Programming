149a8908a48c23ea7803c7df70a50147d466c1b1
prtham312 <pb3488@srmist.edu.in>
2025-04-02 10:41:48 +0530

    Reduce Right

--- ES_JS_Versions.js
+++ ES_JS_Versions.js
@@ -15,3 +15,9 @@ function reduces(arr){
     return arr.reduce((total , num) => total + num , 0)
 }
 console.log(reduces([1,2,3,4,5]))
+
+//reduce right
+function reduce_r(arr){
+    return arr.reduceRight((total,value) => total-value)
+}
+console.log(reduce_r([-10,1,2,3,4,5]))
