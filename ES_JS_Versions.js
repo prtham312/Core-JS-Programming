e2ae9c4c69e200cebe840befa1ec7b7fa2d1741a
prtham312 <pb3488@srmist.edu.in>
2025-04-02 10:39:01 +0530

    Reduce Array

--- ES_JS_Versions.js
+++ ES_JS_Versions.js
@@ -9,3 +9,9 @@ console.log(strict());
 //Char at position
 let str = "Hello World";
 console.log(str.charAt(3));
+
+//reduce array
+function reduces(arr){
+    return arr.reduce((total , num) => total + num , 0)
+}
+console.log(reduces([1,2,3,4,5]))
