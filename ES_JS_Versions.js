2d7bc2deefacba7cd03a853dfef06a4c6f5f6f01
prtham312 <pb3488@srmist.edu.in>
2025-04-02 10:28:59 +0530

    Char At Index

--- ES_JS_Versions.js
+++ ES_JS_Versions.js
@@ -1,6 +1,11 @@
+//Strict Mode
 function strict(){
 "use strict"; //for stricter parsing and error handling
-x = 10;
+let x = 10;
 return x;
 }
 console.log(strict());
+
+//Char at position
+let str = "Hello World";
+console.log(str.charAt(3));
