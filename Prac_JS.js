800bb4444b8a96106ce6da60a7b8ac6fa1b0d549
prtham312 <pb3488@srmist.edu.in>
2025-04-03 11:06:29 +0530

    Enhance WeakMap example in Prac_JS.js with comments and deletion demonstration

--- Prac_JS.js
+++ Prac_JS.js
@@ -1,5 +1,5 @@
 //Weak map
-const weakmap = new WeakMap();
+const weakmap = new WeakMap(); //always has object as a key and and has weak reference to them
 const obj1 = { id : 1}
 const obj2 = { id : 2}
 weakmap.set(obj1 , 'Jacob')  //set the value
@@ -7,3 +7,7 @@ weakmap.set(obj2 , 'Alex')
 console.log(weakmap.get(obj1)); //fetch the value
 console.log(weakmap.get(obj2));
 console.log(weakmap.has(obj2)); // check if object is in weakmap or not
+
+//deleting the weakmap object
+weakmap.delete(obj2)
+console.log(weakmap.has(obj2)); // returns false as object has been delete
